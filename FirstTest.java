ddbbdbabb94e2723a2ce2147669a493de490fe3a
farouk877 <faroukmarhaba1@gmail.com>
2019-09-12 19:13:35 -0400

    Farouk did third commit

--- FirstTest.java
+++ FirstTest.java
@@ -2,5 +2,6 @@ public class FirstTest {
 	public static void main(String[] args) {
 		System.out.println("If you see this, it is good!");
 		System.out.println("Jeremy the G");
+		System.out.println("Farouk got this file after Jeremy");
 	}
 }
