16b3dd6e8cb83e4c9fa84a884f39af35401851d3
Nick <nhutchison3@gatech.edu>
2019-09-12 20:58:18 -0400

    test, nick

--- FirstTest.java
+++ FirstTest.java
@@ -3,6 +3,7 @@ public class FirstTest {
 		System.out.println("If you see this, it is good!");
 		System.out.println("Jeremy the G");
 		System.out.println("Farouk got this file after Jeremy");
-		System.out.println("tes6t");
+		System.out.println("tes6t");\
+		System.out.println("Nick finally fixed his macros. at 8:55.");
 	}
 }
