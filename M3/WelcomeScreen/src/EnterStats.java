8eafc10b86d9b1ee84af4aebee7772103e20b777
jeremyhua18 <jeremyhua18@gmail.com>
2019-09-20 10:02:43 -0400

    adding text field

--- M3/WelcomeScreen/src/EnterStats.java
+++ M3/WelcomeScreen/src/EnterStats.java
@@ -14,6 +14,8 @@ public class EnterStats extends JFrame {
     JRadioButton medium;
     JRadioButton hard;
     ButtonGroup bgDifficulty;
+    JTextField tf1;
+    String input;
 
     EnterStats(String title, WelcomeController c) {
         super(title);
@@ -26,12 +28,18 @@ public class EnterStats extends JFrame {
         medium = new JRadioButton("MEDIUM");
         hard = new JRadioButton("HARD");
         label = new JLabel("This is the stats screen");
+        tf1 = new JTextField();
+        tf1.setBounds(50,200,200,20);
+
+
+
         startButton = new JButton("enter stats");
 
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                c.showWelcome();
+                input = tf1.getText();
+
             }
         });
         bgDifficulty = new ButtonGroup();
@@ -41,9 +49,12 @@ public class EnterStats extends JFrame {
         panel.add(easy);
         panel.add(medium);
         panel.add(hard);
+        panel.add(tf1);
+
 
         add(panel);
         add(label);
         add(startButton);
     }
+
 }
