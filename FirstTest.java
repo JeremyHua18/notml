7949a41e6c27414e5455247f06c18bc1a2d232af
Zuotianyi <55261595+Zuotianyi@users.noreply.github.com>
2019-10-17 12:33:24 -0400

    Delete FirstTest.java

--- FirstTest.java
+++
@@ -1,9 +0,0 @@
-public class FirstTest {
-	public static void main(String[] args) {
-		System.out.println("If you see this, it is good!");
-		System.out.println("Jeremy the G");
-		System.out.println("Farouk got this file after Jeremy");
-		System.out.println("tes6t");\
-		System.out.println("Nick finally fixed his macros. at 8:55.");
-	}
-}
